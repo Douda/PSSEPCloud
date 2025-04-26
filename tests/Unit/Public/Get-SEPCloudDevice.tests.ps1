0df9a6e6b7510658ce1572bebbccdd0eeb05c557
Aurelien BOUMANNE <aurelien.boumanne@gmail.com>
2025-04-26 13:10:58 +0200

    add default template beforeall / afterall values

--- tests/Unit/Public/Get-SEPCloudDevice.tests.ps1
+++ tests/Unit/Public/Get-SEPCloudDevice.tests.ps1
@@ -1,17 +1,25 @@
 BeforeAll {
     $script:moduleName = 'PSSEPCloud'
-    $ProjectPath = "$PSScriptRoot/../../.." | Convert-Path
 
-    # Redirect all streams to $null, except the error stream (stream 2)
-    & "$ProjectPath/build.ps1" -Tasks 'noop' 2>&1 4>&1 5>&1 6>&1 > $null
+    # If the module is not found, run the build task 'noop'.
+    if (-not (Get-Module -Name $script:moduleName -ListAvailable)) {
+        # Redirect all streams to $null, except the error stream (stream 2)
+        & "$PSScriptRoot/../../build.ps1" -Tasks 'noop' 2>&1 4>&1 5>&1 6>&1 > $null
+    }
 
-    # Not Forcing module import
-    # See reasons behind Pester v5 : https://github.com/pester/Pester/discussions/2109
-    # Requires to build module prior running Pester
-    Import-Module -Name $script:moduleName -ErrorAction 'Stop'
+    # Re-import the module using force to get any code changes between runs.
+    Import-Module -Name $script:moduleName -Force -ErrorAction 'Stop'
+
+    $PSDefaultParameterValues['InModuleScope:ModuleName'] = $script:moduleName
+    $PSDefaultParameterValues['Mock:ModuleName'] = $script:moduleName
+    $PSDefaultParameterValues['Should:ModuleName'] = $script:moduleName
 }
 
 AfterAll {
+    $PSDefaultParameterValues.Remove('Mock:ModuleName')
+    $PSDefaultParameterValues.Remove('InModuleScope:ModuleName')
+    $PSDefaultParameterValues.Remove('Should:ModuleName')
+
     Remove-Module -Name $script:moduleName
 }
 
